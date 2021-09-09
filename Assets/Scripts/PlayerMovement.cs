2940990246daff87e40b8c8d4db55686d940a0ec
yaakgold <yaakovgoldberg007@gmail.com>
2021-09-09 13:24:41 -0600

    Start of basics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Public Vars
+    public float speed;
+    #endregion
+
+    #region Private Vars
+    private Vector3 movement;
+    #endregion
+
+    #region Unity Callbacks
+    private void Start()
     {
-        
+        movement = Vector3.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position += movement * speed * Time.deltaTime;
     }
+    #endregion
+
+    #region Input System
+    public void OnMove(InputValue value)
+    {
+        movement.x = value.Get<Vector2>().x;
+        movement.z = value.Get<Vector2>().y;
+    }
+    #endregion
+
 }
