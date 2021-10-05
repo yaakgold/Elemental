0b61ed1923ee92d03dcb3a66b7c8c7141b9acf56
BadHatHoffman <EHoffman@student.neumont.edu>
2021-10-05 13:53:30 -0600

    Added GameObject Abilities
    Added the 3 game object abilities to the abilities script

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Abilities : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    GameObject firstAbility;
+    GameObject secondAbility;
+    GameObject thirdAbility;
+    
 }
