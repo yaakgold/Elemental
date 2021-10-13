0c5e531debd0b7eca282ff195496563345ea81c0
BadHatHoffman <EHoffman@student.neumont.edu>
2021-10-13 12:00:54 -0600

    Fixed XP Testing Button

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -72,7 +72,7 @@ namespace StarterAssets
 
 #if !UNITY_IOS || !UNITY_ANDROID
 
-        private void OnApplicationFocus(bool hasFocus)
+        /*private void OnApplicationFocus(bool hasFocus)
         {
             SetCursorState(cursorLocked);
         }
@@ -80,7 +80,7 @@ namespace StarterAssets
         private void SetCursorState(bool newState)
         {
             Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-        }
+        }*/
 
 #endif
 
